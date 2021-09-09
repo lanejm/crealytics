121e739f91727e200daf0aec3870d9dfd97cb0ce
lanejm <67156874+lanejm@users.noreply.github.com>
2021-09-09 12:52:01 -0400

    main pages made

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SearchForm from "../components/SearchFrom";
+
+const Home = () => {
+  return (
+    <main>
+      <SearchForm />
+    </main>
+  );
+};
+
+export default Home;
